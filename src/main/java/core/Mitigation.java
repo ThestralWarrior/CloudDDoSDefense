e145769f562eece743c8c1d6828636fa770e8ead
ThestralWarrior <ashbroezio@outlook.com>
2024-09-19 10:24:29 +0530

    mitigation mechanisms

--- src/main/java/core/Mitigation.java
+++ src/main/java/core/Mitigation.java
@@ -1,4 +1,20 @@
 package core;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Mitigation {
+    private static final Map<String, Boolean> blockedIPs = new ConcurrentHashMap();
+    public static boolean isBlocked(String ipAddress) {
+        synchronized (blockedIPs) {
+            return blockedIPs.getOrDefault(ipAddress, false);
+        }
+    }
+    public static void mitigate(String ipAddress) {
+        // AlertSystem.alert();
+        synchronized (blockedIPs) {
+            System.out.println("Taking mitigating measures - blacklisting IP Address " + ipAddress);
+            blockedIPs.put(ipAddress, true);
+        }
+    }
 }
